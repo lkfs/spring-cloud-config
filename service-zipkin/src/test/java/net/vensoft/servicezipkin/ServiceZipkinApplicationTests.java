a69e3cd9551496ee46f145020d866a6607ce8f95
lkfs <lkfs@163.com>
2020-11-20 16:14:47 +0800

    服务链路追踪

---
+++ service-zipkin/src/test/java/net/vensoft/servicezipkin/ServiceZipkinApplicationTests.java
@@ -0,0 +1,13 @@
+package net.vensoft.servicezipkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceZipkinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
